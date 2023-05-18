4cf2371cd28a69089b0a8c53d7d346df13614784
AlexeyRychagov <alexeyrychagov.work@yandex.ru>
2023-05-18 13:03:15 +0300

    add linear progress
    Signed-off-by: AlexeyRychagov <alexeyrychagov.work@yandex.ru>

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,69 +1,26 @@
-import { Box, Slider, Typography } from "@mui/material";
+import { Box, LinearProgress, Slider, Typography } from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { removeCurrentLink } from "../../store/slice/inputSlice";
 import Pause from "../../image/Pause.svg";
 import Play from "../../image/Play.svg";
+import usePlayer from "../../hooks/usePlayer";
 
 export default function Player() {
-  const currentLink = useSelector((state) => state.link.currentLink);
-  const dispatch = useDispatch();
-  const [playerStatus, setplayerStatus] = useState(false);
-  const player = useRef(null);
-  const [volume, setVolume] = useState(1);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [duration, setDuration] = useState(0);
-
-  useEffect(() => {
-    const audioElement = player.current;
-
-    const handleLoadedData = () => {
-      setDuration(audioElement.duration);
-    };
-
-    const handleTimeUpdate = () => {
-      setCurrentTime(audioElement.currentTime);
-    };
-
-    audioElement.addEventListener("loadeddata", handleLoadedData);
-    audioElement.addEventListener("timeupdate", handleTimeUpdate);
-
-    return () => {
-      audioElement.removeEventListener("loadeddata", handleLoadedData);
-      audioElement.removeEventListener("timeupdate", handleTimeUpdate);
-    };
-  }, []);
-
-  const handleTimeChange = (event, newValue) => {
-    const audioElement = player.current;
-    audioElement.currentTime = newValue;
-    setCurrentTime(newValue);
-  };
-
-  const backToInputHandler = () => {
-    dispatch(removeCurrentLink());
-  };
-
-  const playerStatusHandler = () => {
-    setplayerStatus((prev) => !prev);
-    playerStatus ? player.current.pause() : player.current.play();
-  };
-  const handleVolumeChange = (event, newValue) => {
-    const audioElement = player.current;
-    audioElement.volume = newValue;
-    setVolume(newValue);
-  };
-  function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds) % 60;
-
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
-
-    return `${formattedMinutes}:${formattedSeconds}`;
-  }
-
+  const {
+    backToInputHandler,
+    playerStatus,
+    playerStatusHandler,
+    duration,
+    handleTimeChange,
+    formatTime,
+    currentTime,
+    volume,
+    handleVolumeChange,
+    player,
+    currentLink,
+  } = usePlayer();
   return (
     <Box>
       <Typography
@@ -89,6 +46,7 @@ export default function Player() {
           position: "relative",
         }}
       >
+        <LinearProgress color='secondary' />
         <Box ml={"20px"} pt={"20px"}>
           {playerStatus ? (
             <img
@@ -123,7 +81,7 @@ export default function Player() {
               color: "#ADACAD",
               "& .MuiSlider-thumb": {
                 backgroundColor: "white",
-				transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
+                transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
               },
             }}
           />
@@ -144,20 +102,20 @@ export default function Player() {
             min={0}
             max={1}
             step={0.1}
-			sx={{
-				width: '252px',
-				color: 'white',
-				'& .MuiSlider-track':{
-					color: 'black'
-				},
-				"& .MuiSlider-thumb": {
-					backgroundColor: "black",
-					transition: '0.3s cubic-bezier(.47,1.64,.41,.8)',
-					width: '1rem',
-					height: '0.75rem',
-					borderRadius: 0
-				  },
-			}}
+            sx={{
+              width: "252px",
+              color: "white",
+              "& .MuiSlider-track": {
+                color: "black",
+              },
+              "& .MuiSlider-thumb": {
+                backgroundColor: "black",
+                transition: "0.3s cubic-bezier(.47,1.64,.41,.8)",
+                width: "1rem",
+                height: "0.75rem",
+                borderRadius: 0,
+              },
+            }}
           />
         </Box>
       </Box>
---
+++ src/hooks/usePlayer.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeCurrentLink } from '../store/slice/inputSlice';
+
+export default function usePlayer() {
+	const currentLink = useSelector((state) => state.link.currentLink);
+	const dispatch = useDispatch();
+	const [playerStatus, setplayerStatus] = useState(false);
+	const player = useRef(null);
+	const [volume, setVolume] = useState(1);
+	const [currentTime, setCurrentTime] = useState(0);
+	const [duration, setDuration] = useState(0);
+  
+	useEffect(() => {
+	  const audioElement = player.current;
+  
+	  const handleLoadedData = () => {
+		setDuration(audioElement.duration);
+	  };
+  
+	  const handleTimeUpdate = () => {
+		setCurrentTime(audioElement.currentTime);
+	  };
+  
+	  audioElement.addEventListener("loadeddata", handleLoadedData);
+	  audioElement.addEventListener("timeupdate", handleTimeUpdate);
+  
+	  return () => {
+		audioElement.removeEventListener("loadeddata", handleLoadedData);
+		audioElement.removeEventListener("timeupdate", handleTimeUpdate);
+	  };
+	}, []);
+  
+	const handleTimeChange = (event, newValue) => {
+	  const audioElement = player.current;
+	  audioElement.currentTime = newValue;
+	  setCurrentTime(newValue);
+	};
+  
+	const backToInputHandler = () => {
+	  dispatch(removeCurrentLink());
+	};
+  
+	const playerStatusHandler = () => {
+	  setplayerStatus((prev) => !prev);
+	  playerStatus ? player.current.pause() : player.current.play();
+	};
+	const handleVolumeChange = (event, newValue) => {
+	  const audioElement = player.current;
+	  audioElement.volume = newValue;
+	  setVolume(newValue);
+	};
+	function formatTime(seconds) {
+	  const minutes = Math.floor(seconds / 60);
+	  const remainingSeconds = Math.floor(seconds) % 60;
+  
+	  const formattedMinutes = String(minutes).padStart(2, "0");
+	  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+  
+	  return `${formattedMinutes}:${formattedSeconds}`;
+	}
+   return { backToInputHandler, playerStatus, playerStatusHandler,duration, handleTimeChange,formatTime, currentTime, volume, handleVolumeChange, player , currentLink}
+}
